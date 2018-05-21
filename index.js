6122230c7204cd8c1b08736f798f0794bf8e4eec
unknown <mrhdigital@yahoo.com>
2018-05-21 14:18:59 -0500

    prompt working

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var Word = require("./word.js")
-var inquirer = require("inquirer")
+var Word = require("./Word.js");
+var inquirer = require("inquirer");
 
 var wordArray = [
     "toucan","parrot", "hornbill", 
@@ -7,15 +7,36 @@ var wordArray = [
     "heron","cuckoo"];
     var totalGuesses = "";
     var target = "";
+    var playedAlready = false;
 
 
+    var selectedRandomWord = function() {
+        return wordArray[Math.floor(Math.random() *10)];
+
+    };
+
+    const inquire = function(){
+        inquirer.prompt([
+            {
+                type: "confirm",
+                message: `Do you want to play ${playedAlready ? 'again?' : 'a round?'}`,
+                name: "play"
+            }
+        ]).then(function(answer){
+            if (answer.play) {
+                console.log("Let's play!");
+                playedAlready = true;
+                playGame();    
+            } else {
+                console.log('Ok! See ya later!');
+            }
+        }).catch(function(error){
+            console.log("Oh boy, it broke: " + error);
+        });
+    };
     
+    // Function Calls
+    
+    inquire();
 
- function startGame() {
-     randonWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-     totalGuesses = randonWord.length +10;
-     target = new Word(randomWord);
-     console.log(target);
- }   
-
- startGame();
+ 
